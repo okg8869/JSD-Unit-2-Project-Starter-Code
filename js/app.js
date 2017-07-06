71d1f7472e93ae182be0191fffd477a765ef5307
Oliver Gillette <olivergillette@gmail.com>
2017-07-05 21:26:37 -0400

    First Iteration RSS
    The first iteration, 1 feed coming in, some requirements being met, not
    completed yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 /*
   Please add all Javascript code to this file.
 */
+$(document).ready(function(){
+	var rssApp = {};
+
+	rssApp.updateFeed = function(item){
+		var source = $('#rss-template').html();
+		var template = Handlebars.compile(source);
+		return template(item);
+	};
+
+	$('#popUp').removeClass('hidden');
+	var redditUrl = "https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/top.json";
+	var request = $.ajax({
+		url: redditUrl
+	});
+	request.done(function(results){
+		$('#popUp').addClass('hidden');
+		var rssResults = results.data.children;
+		for(i=0;i<rssResults.length;i++){
+			var postInfo = {
+				ups:rssResults[i].data.ups,
+				author:rssResults[i].data.author,
+				image:rssResults[i].data.thumbnail,
+				title:rssResults[i].data.title,
+				link:rssResults[i].data.url
+			}
+			var rssOnPage = rssApp.updateFeed(postInfo);
+			$('#main').prepend(rssOnPage);
+
+			$('#main').children(".article").on("click", function (){
+				$('#popUp').removeClass('loader');
+				$('#popUp').removeClass('hidden');
+			})
+		};
+	});
+
+});
